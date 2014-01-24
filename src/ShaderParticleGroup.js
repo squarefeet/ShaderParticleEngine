9b1dcb670f92a5b54ea4a8c2847c272680dddc02
Andrew Ray <delvarworld@gmail.com>
2014-01-23 22:59:30 -0800

    Fixing angularAlignVelocity for emitters not at origin
    Before the angle was always pointing to the origin. Addresses issue #29

--- src/ShaderParticleGroup.js
+++ src/ShaderParticleGroup.js
@@ -481,7 +481,7 @@ ShaderParticleGroup.shaders = {
                 'vec4 pos = GetPos();',
 
                 'if( angleAlignVelocity == 1.0 ) {',
-                    'vAngle = -atan(pos.y, pos.x);',
+                    'vAngle = -atan(pos.y - position.y, pos.x - position.x);',
                 '}',
                 'else {',
                     'vAngle = 0.0;',
