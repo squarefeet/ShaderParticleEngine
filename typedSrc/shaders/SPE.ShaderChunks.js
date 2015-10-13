68f555eb0b5dd1bec581fbce39c9ef8b366c3ebb
Luke Moody <luke.t.moody@googlemail.com>
2015-10-13 20:00:03 +0100

    Moved vertex shader from HTML test file into SPE.shaders.vertex. Split it up into manageable chunks

--- typedSrc/shaders/SPE.ShaderChunks.js
+++ typedSrc/shaders/SPE.ShaderChunks.js
@@ -1,15 +1,180 @@
 SPE.shaderChunks = {
-    getFloatOverLifetime: ( function() {
-        var src = [
-            'float get%name%OverLifetime() {',
-            '	',
-            '}'
-        ];
+    defines: [
+        // '#define PI 3.141592653589793',
+        // '#define PI_2 6.283185307179586',
+        '#define PACKED_COLOR_SIZE 256.0',
+        '#define PACKED_COLOR_DIVISOR 255.0'
+    ].join( '\n' ),
 
-        return function( name ) {
-            var shader = src.replace( '%name%', name );
+    uniforms: [
+        'uniform float deltaTime;',
+        'uniform float runTime;'
+    ].join( '\n' ),
 
-            return shader;
-        };
-    }() )
+    attributes: [
+        'attribute vec4 acceleration;',
+        'attribute vec3 velocity;',
+        'attribute vec4 rotation;',
+        'attribute vec3 rotationCenter;',
+        'attribute vec4 params;',
+        'attribute vec4 size;',
+        'attribute vec4 angle;',
+        'attribute vec4 color;',
+        'attribute vec4 opacity;'
+    ].join( '\n' ),
+
+    varyings: [
+        'varying vec4 vColor;',
+        'varying float vAngle;',
+        // 'varying float vIsAlive;',
+    ].join( '\n' ),
+
+    branchAvoidanceFunctions: [
+        // Branch-avoiding comparison fns
+        // - http://theorangeduck.com/page/avoiding-shader-conditionals
+        'float when_gt(float x, float y) {',
+        '    return max(sign(x - y), 0.0);',
+        '}',
+
+        'float when_lt(float x, float y) {',
+        '    return min( max(1.0 - sign(x - y), 0.0), 1.0 );',
+        '}',
+
+        'float when_eq( float x, float y ) {',
+        '    return 1.0 - abs( sign( x - y ) );',
+        '}',
+
+        'float when_ge(float x, float y) {',
+        '  return 1.0 - when_lt(x, y);',
+        '}',
+
+        'float when_le(float x, float y) {',
+        '  return 1.0 - when_gt(x, y);',
+        '}',
+
+        // Branch-avoiding logical operators
+        // (to be used with above comparison fns)
+        'float and(float a, float b) {',
+        '    return a * b;',
+        '}',
+
+        'float or(float a, float b) {',
+        '    return min(a + b, 1.0);',
+        '}',
+    ].join( '\n' ),
+
+    unpackColor: [
+        // From:
+        // - http://stackoverflow.com/a/12553149
+        // - https://stackoverflow.com/questions/22895237/hexadecimal-to-rgb-values-in-webgl-shader
+        'vec3 unpackColor( in float hex ) {',
+        '   vec3 c = vec3( 0.0 );',
+
+        '   float r = mod( (hex / PACKED_COLOR_SIZE / PACKED_COLOR_SIZE), PACKED_COLOR_SIZE );',
+        '   float g = mod( (hex / PACKED_COLOR_SIZE), PACKED_COLOR_SIZE );',
+        '   float b = mod( hex, PACKED_COLOR_SIZE );',
+
+        '   c.r = r / PACKED_COLOR_DIVISOR;',
+        '   c.g = g / PACKED_COLOR_DIVISOR;',
+        '   c.b = b / PACKED_COLOR_DIVISOR;',
+
+        '   return c;',
+        '}',
+    ].join( '\n' ),
+
+    floatOverLifetime: [
+        'float getFloatOverLifetime( in float positionInTime, in vec4 attr ) {',
+        '    float value = 0.0;',
+        '    float deltaAge = positionInTime * float( VALUE_OVER_LIFETIME_LENGTH );',
+        '    float fIndex = 0.0;',
+        '    float shouldApplyValue = 0.0;',
+
+        // This might look a little odd, but it's quite elegant. Uses
+        // basic maths to avoid branching. Nice.
+        //
+        // Take a look at the branch-avoidance functions defined above,
+        // and be sure to check out The Orange Duck site where I got this
+        // from (link above).
+        '    for( int i = 0; i < VALUE_OVER_LIFETIME_LENGTH - 1; ++i ) {',
+        '       fIndex = float( i );',
+        '       shouldApplyValue = and( when_ge( deltaAge, fIndex ), when_lt( deltaAge, fIndex + 1.0 ) );',
+        '       value += shouldApplyValue * mix( attr[ i ], attr[ i + 1 ], deltaAge - fIndex );',
+        '    }',
+
+        '    return value;',
+        '}',
+    ].join( '\n' ),
+
+    colorOverLifetime: [
+        'vec3 getColorOverLifetime( in float positionInTime, in vec3 color1, in vec3 color2, in vec3 color3, in vec3 color4 ) {',
+        '    vec3 value = vec3( 0.0 );',
+        '    value.x = getFloatOverLifetime( positionInTime, vec4( color1.x, color2.x, color3.x, color4.x ) );',
+        '    value.y = getFloatOverLifetime( positionInTime, vec4( color1.y, color2.y, color3.y, color4.y ) );',
+        '    value.z = getFloatOverLifetime( positionInTime, vec4( color1.z, color2.z, color3.z, color4.z ) );',
+        '    return value;',
+        '}',
+    ].join( '\n' ),
+
+    paramFetchingFunctions: [
+        'float getAlive() {',
+        '   return params.x;',
+        '}',
+
+        'float getAge() {',
+        '   return params.y;',
+        '}',
+
+        'float getMaxAge() {',
+        '   return max( getAge(), params.z );',
+        '}',
+
+        'float getWiggle() {',
+        '   return params.w;',
+        '}',
+    ].join( '\n' ),
+
+    forceFetchingFunctions: [
+        'vec4 getPosition( in float age ) {',
+        '   return modelViewMatrix * vec4( position, 1.0 );',
+        '}',
+
+        'vec3 getVelocity( in float age ) {',
+        '   return velocity * age;',
+        '}',
+
+        'vec3 getAcceleration( in float age ) {',
+        '   return acceleration.xyz * age;',
+        '}',
+    ].join( '\n' ),
+
+
+    rotationFunctions: [
+        // Huge thanks to:
+        // - http://www.neilmendoza.com/glsl-rotation-about-an-arbitrary-axis/
+        'mat4 getRotationMatrix( in vec3 axis, in float angle) {',
+        '    axis = normalize(axis);',
+        '    float s = sin(angle);',
+        '    float c = cos(angle);',
+        '    float oc = 1.0 - c;',
+
+        '    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,',
+        '                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,',
+        '                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,',
+        '                0.0,                                0.0,                                0.0,                                1.0);',
+        '}',
+
+        'vec3 getRotation( in vec3 pos, in float positionInTime ) {',
+        '   vec3 axis = unpackColor( rotation.x );',
+        '   vec3 center = rotationCenter;',
+        '   vec3 translated;',
+        '   mat4 rotationMatrix;',
+
+        '   float angle = 0.0;',
+        '   angle += when_eq( rotation.z, 0.0 ) * rotation.y;',
+        '   angle += when_gt( rotation.z, 0.0 ) * mix( 0.0, rotation.y, positionInTime * 1.35 );',
+        '   translated = rotationCenter - pos;',
+        '   rotationMatrix = getRotationMatrix( axis, angle );',
+        '   return center + vec3( rotationMatrix * vec4( translated, 1.0 ) );',
+        '}',
+    ].join( '\n' )
 };
--- typedSrc/shaders/SPE.shaders.js
+++ typedSrc/shaders/SPE.shaders.js
@@ -1,7 +1,128 @@
 SPE.shaders = {
     vertex: [
+        SPE.shaderChunks.defines,
+        SPE.shaderChunks.uniforms,
+        SPE.shaderChunks.attributes,
+        SPE.shaderChunks.varyings,
 
+        SPE.shaderChunks.branchAvoidanceFunctions,
+        SPE.shaderChunks.unpackColor,
+        SPE.shaderChunks.floatOverLifetime,
+        SPE.shaderChunks.colorOverLifetime,
+        SPE.shaderChunks.paramFetchingFunctions,
+        SPE.shaderChunks.forceFetchingFunctions,
+        SPE.shaderChunks.rotationFunctions,
+
+
+        'void main() {',
+
+
+        //
+        // Setup...
+        //
+        '	float age = getAge();',
+        '	float alive = getAlive();',
+        '	float maxAge = getMaxAge();',
+        '	float positionInTime = (age / maxAge);',
+        '	float wiggleAmount = positionInTime * getWiggle();',
+        '	float wiggleSin = sin( wiggleAmount );',
+        '	float wiggleCos = cos( wiggleAmount );',
+        '	float isAlive = when_gt( alive, 0.0 );',
+
+
+
+        // Save the value is isAlive to a varying for
+        // access in the fragment shader
+        // '	vIsAlive = isAlive;',
+
+
+
+        //
+        // Forces
+        //
+
+        // Get forces & position
+        '	vec3 vel = getVelocity( age );',
+        '	vec3 accel = getAcceleration( age );',
+        '	vec3 force = vec3( 0.0 );',
+        '	vec3 pos = vec3( position );',
+
+        // Can't figure out why positionInTime needs to be multiplied
+        // by 0.6 to give the desired result...Should be value between
+        // 0.0 and 1.0!?
+        '	float drag = (1.0 - (positionInTime * 0.6) * acceleration.w);',
+
+        // Integrate forces...
+        '	force += vel;',
+        '	force *= drag;',
+        '	force += accel * age;',
+        '	pos += force;',
+
+
+        // Wiggly wiggly wiggle!
+        '	pos.x += wiggleSin;',
+        '	pos.y += wiggleCos;',
+        '	pos.z += wiggleSin;',
+
+
+        // Rotate the emitter around it's central point
+        '	pos = getRotation( pos, positionInTime );',
+
+        // Convert pos to a world-space value
+        '	vec4 mvPos = modelViewMatrix * vec4( pos, 1.0 );',
+
+        // Determine point size.
+        '	float pointSize = getFloatOverLifetime( positionInTime, size ) * isAlive;',
+
+        // Determine perspective
+        // TODO: Accept camera here via `scale` uniform
+        '	#ifdef HAS_PERSPECTIVE',
+        '		float perspective = 300.0 / length( mvPos.xyz );',
+        '	#else',
+        '		float perspective = 1.0;',
+        '	#endif',
+
+        // Apply perpective to pointSize value
+        '	float pointSizePerspective = pointSize * perspective;',
+
+
+        //
+        // Appearance
+        //
+
+        // Determine color and opacity for this particle
+        '	 #ifdef COLORIZE',
+        '    	vec3 c = isAlive * getColorOverLifetime(',
+        '    		positionInTime,',
+        '    		unpackColor( color.x ),',
+        '    		unpackColor( color.y ),',
+        '    		unpackColor( color.z ),',
+        '    		unpackColor( color.w )',
+        '    	);',
+        '    #else',
+        '    	vec3 c = vec3(1.0);',
+        '	 #endif',
+
+        '    float o = isAlive * getFloatOverLifetime( positionInTime, opacity );',
+
+        // Assign color to vColor varying.
+        '	vColor = vec4( c, o );',
+
+        // Determine angle
+        '	vAngle = isAlive * getFloatOverLifetime( positionInTime, angle );',
+
+
+
+        //
+        // Write values
+        //
+
+        // Set PointSize according to size at current point in time.
+        '	gl_PointSize = pointSizePerspective;',
+        '	gl_Position = projectionMatrix * mvPos;',
+        '}',
 
     ].join( '\n' ),
+
     fragment: [].join( '\n' )
 };
