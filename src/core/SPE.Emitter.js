06cf30caee7164dc28e7a0c75817ca5aa31391c0
Kevin <titansoftime@gmail.com>
2022-05-27 23:25:27 -0500

    Change THREE.Math.generateUUID to THREE.MathUtils.generateUUID
    Looks like THREE.Math got changed to THREE.MathUtils at some point. This is one of 2 lines that needed to be updated.

--- src/core/SPE.Emitter.js
+++ src/core/SPE.Emitter.js
@@ -176,7 +176,7 @@ SPE.Emitter = function( options ) {
         console.warn( 'onParticleSpawn has been removed. Please set properties directly to alter values at runtime.' );
     }
 
-    this.uuid = THREE.Math.generateUUID();
+    this.uuid = THREE.MathUtils.generateUUID();
 
     this.type = utils.ensureTypedArg( options.type, types.NUMBER, SPE.distributions.BOX );
 
